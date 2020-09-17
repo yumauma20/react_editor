c2247078ba757043726ad66b8de99e5ff5b1c677
yumauma20 <71yuma10@gmail.com>
2020-09-17 19:11:31 +0900

    React Hooksを使った状態管理

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+const { useState } = React
+
 const Header = styled.header`
   font-size: 1.5rem;
   height: 2rem;
@@ -43,13 +45,19 @@ const Preview = styled.div`
   width: 50vw;
 `
 export const Editor: React.FC = () => {
+  const [text, setText] = useState<string>('')
   return (
     <>
       <Header>
         Markdown Editor
       </Header>
       <Wrapper>
-        <TextArea value="テキスト入力エリア"/>
+        <TextArea
+          onChange={(event) => {
+            setText(event.target.value)
+          }}
+          value={text}
+        />
         <Preview>プレビューエリア</Preview>
       </Wrapper>
     </>
