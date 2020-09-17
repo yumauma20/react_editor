308effbcfbca3b16a2b0bf44e4f9ddd9398ead46
yumauma20 <71yuma10@gmail.com>
2020-09-17 22:21:17 +0900

    WebStorage導入

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -44,8 +44,10 @@ const Preview = styled.div`
   top: 0;
   width: 50vw;
 `
+const StorageKey = 'pages/editor:text'
+
 export const Editor: React.FC = () => {
-  const [text, setText] = useState<string>('')
+  const [text, setText] = useState<string>(localStorage.getItem(StorageKey) || '')
   return (
     <>
       <Header>
@@ -54,7 +56,9 @@ export const Editor: React.FC = () => {
       <Wrapper>
         <TextArea
           onChange={(event) => {
-            setText(event.target.value)
+            const changedText = event.target.value
+            localStorage.setItem(StorageKey, changedText)
+            setText(changedText)
           }}
           value={text}
         />
