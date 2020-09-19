b71d6045273495bb4f802315e97d7db2d9bd7ad0
yumauma20 <71yuma10@gmail.com>
2020-09-19 10:56:04 +0900

    DB設定

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import * as ReactMarkdown from 'react-markdown'
+import { putMemo } from '../indexeddb/memos'
+import { Button } from '../components/button'
 
 const { useState } = React
 
 const Header = styled.header`
+  align-content: center;
+  display: flex;
   font-size: 1.5rem;
   height: 2rem;
+  justify-content: space-between;
   left: 0;
   line-height: 2rem;
   padding: 0.5rem 1rem;
   position: fixed;
   right: 0;
   top: 0;
+`
+
+const HeaderControl = styled.div`
+  height: 2rem;
+  display: flex;
+  align-content: center;
 `
 
 const Wrapper = styled.div`
@@ -49,10 +60,20 @@ const StorageKey = 'pages/editor:text'
 
 export const Editor: React.FC = () => {
   const [text, setText] = useState<string>(localStorage.getItem(StorageKey) || '')
+
+  const saveMemo = (): void => {
+    putMemo('TITLE', text)
+  }
+
   return (
     <>
       <Header>
         Markdown Editor
+        <HeaderControl>
+          <Button onClick={saveMemo}>
+            保存する
+          </Button>
+        </HeaderControl>
       </Header>
       <Wrapper>
         <TextArea
